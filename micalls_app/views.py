cf296b38e13b5c1e4df6c9cb19b0769ecfb7f9ee
= <=>
2024-05-11 18:33:30 +0100

    update

--- micalls_app/views.py
+++ micalls_app/views.py
@@ -5,6 +5,9 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+def home(request):
+    return render(request, 'home.html')
+
 def index(request):
     return render(request, 'index.html')
 
