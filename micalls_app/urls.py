4422d9504f8c16511ad37d32d969295c2a459b78
= <=>
2024-05-11 18:54:35 +0100

    updated with homepage

--- micalls_app/urls.py
+++ micalls_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("home/", views.home, name="home"),
+    path("", views.home, name="home"),
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
     path('dashboard/', views.dashboard, name='dashboard'),
